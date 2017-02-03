692c79641a61497c896f7c4803c9235ce92e04c1
Rasmus Nilsson <zawroli@hotmail.com>
2017-02-03 13:31:42 +0100

    Mer design på första skärmen och på andra.

--- mainjavascript.js
+++ mainjavascript.js
@@ -7,22 +7,6 @@ function switchWindow(screen) {
 		$(".secondScreen").css("display", 'flex');
 	}
 }
-
 $(".firstScreen h2").on("click", function() {
 	switchWindow("second");
 });
-
-$(".secondScreen h2").on("click", function() {
-	switchWindow("first");
-});
-
-$(".secondScreen button").on("click", function() {
-	answersOut()
-});
-
-
-function answersOut() {
-	$(".questionDiv ul li:nth-of-type(1)").transition({
-		opacity: 0
-	});
-}
