60fd0165eb96e4a5cf5db2a32eb6075a18f167ff
Rasmus Nilsson <zawroli@hotmail.com>
2017-02-05 19:35:49 +0100

    Design på svarsknapparna

--- mainjavascript.js
+++ mainjavascript.js
@@ -14,7 +14,7 @@ $(".firstScreen h2").on("click", function() {
 var questions = {}
 
 function questionGenerator(list) { // tar en lista med frågan först, sen svar
-	this.question = list[0]; // sätter frågan först
+	this.question = list[0]; // sätter frågan först i objektet
 	for (var i = 1; i < list.length; i++) { // ger varje svar ett namn och sitt svar
 		var name = "answer" + (i - 1);
 		this[name] = list[i];
@@ -22,19 +22,20 @@ function questionGenerator(list) { // tar en lista med frågan först, sen svar
 	
 }
 
-questions.question1 = new questionGenerator(["Vilket håll åker bussen", "Höger", "Vänster", "Står still", "Vet inte"]);
+questions.question1 = new questionGenerator(["Vilket håll åker bussen?", "Höger", "Vänster", "Står still", "Vet inte"]);
+questions.question2 = new questionGenerator(["Vilket håll åker inte bussen?", "Höger", "Vänster", "Står still", "Vet inte"]);
 
 function importNewQuestion() {
 	var questionLength = Object.keys(questions.question1).length; //ger längden på objektet med frågorna i
+	$(".secondScreen div div p").text(questions.question1.question); //uppdaterar frågan
 	for(var i = 1; i < questionLength; i++) {
 		var li = ".questionDiv ul li:nth-of-type(" + i + ")";
 		$(".questionDiv ul").append("<li></li>"); //lägger till en li
 		$(li).text(questions.question1["answer" + (i - 1)]); //ger li:n text från korrekt fråga
 		$(li).on("click",function() { //onclick style
-			$(".questionDiv ul li").css("background-color", '');
-			$(".questionDiv ul li").css("color", '');
-			$(this).css("background-color", '#29568f');
-			$(this).css("color", 'white');
+			$(".questionDiv ul li").removeClass("selected");
+			$(this).addClass("selected");
+			// $(".questionDiv ul li:nth-of-type(" + (i - 2) + ")").addClass("border");
 		});
 	}
 }
