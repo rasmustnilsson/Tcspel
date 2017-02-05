a5ed22055d4396f33a33e22e8bc29b16ac40948c
Rasmus Nilsson <zawroli@hotmail.com>
2017-02-05 18:21:48 +0100

    Började med importNewQuestion funktionen, och lade till uft-8

--- mainjavascript.js
+++ mainjavascript.js
@@ -1,9 +1,9 @@
 function switchWindow(screen) {
 	if(screen == "first") {
-		$(".secondScreen").css("display", 'none');
+		$("body > div").css("display", 'none');
 		$(".firstScreen").css("display", '');
 	} else if (screen == "second"){
-		$(".firstScreen").css("display", 'none');
+		$("body > div").css("display", 'none');
 		$(".secondScreen").css("display", 'flex');
 	}
 }
@@ -13,13 +13,29 @@ $(".firstScreen h2").on("click", function() {
 
 var questions = {}
 
-function questionGenerator(list) {
-	this.question = list[0];
-	for (var i = 1; i < list.length; i++) {
+function questionGenerator(list) { // tar en lista med frågan först, sen svar
+	this.question = list[0]; // sätter frågan först
+	for (var i = 1; i < list.length; i++) { // ger varje svar ett namn och sitt svar
 		var name = "answer" + (i - 1);
 		this[name] = list[i];
 	}
 	
 }
 
-questions.question1 = new questionGenerator([1,2,3,4]);
+questions.question1 = new questionGenerator(["Vilket håll åker bussen", "Höger", "Vänster", "Står still", "Vet inte"]);
+
+function importNewQuestion() {
+	var questionLength = Object.keys(questions.question1).length; //ger längden på objektet med frågorna i
+	for(var i = 1; i < questionLength; i++) {
+		var li = ".questionDiv ul li:nth-of-type(" + i + ")";
+		$(".questionDiv ul").append("<li></li>"); //lägger till en li
+		$(li).text(questions.question1["answer" + (i - 1)]); //ger li:n text från korrekt fråga
+		$(li).on("click",function() { //onclick style
+			$(".questionDiv ul li").css("background-color", '');
+			$(".questionDiv ul li").css("color", '');
+			$(this).css("background-color", '#29568f');
+			$(this).css("color", 'white');
+		});
+	}
+}
+importNewQuestion();
