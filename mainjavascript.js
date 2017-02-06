553c3317a73de2e2eb7a11116cc4884d406fbea5
Rasmus Nilsson <zawroli@hotmail.com>
2017-02-06 14:07:48 +0100

    Fixade med importNEwQuestions funktionen

--- mainjavascript.js
+++ mainjavascript.js
@@ -12,14 +12,15 @@ $(".firstScreen h2").on("click", function() {
 });
 
 var questions = {}
+var i = 1;
+
 
 function questionGenerator(list) { // tar en lista med frågan först, sen svar
 	this.question = list[0]; // sätter frågan först i objektet
-	for (var i = 1; i < list.length; i++) { // ger varje svar ett namn och sitt svar
+	for (i = 1; i < list.length; i++) { // ger varje svar ett namn och sitt svar
 		var name = "answer" + (i - 1);
 		this[name] = list[i];
 	}
-	
 }
 
 questions.question1 = new questionGenerator(["Vilket håll åker bussen?", "Höger", "Vänster", "Står still", "Vet inte"]);
@@ -28,14 +29,15 @@ questions.question2 = new questionGenerator(["Vilket håll åker inte bussen?", 
 function importNewQuestion() {
 	var questionLength = Object.keys(questions.question1).length; //ger längden på objektet med frågorna i
 	$(".secondScreen div div p").text(questions.question1.question); //uppdaterar frågan
-	for(var i = 1; i < questionLength; i++) {
+	for(i = 1; i < questionLength; i++) {
 		var li = ".questionDiv ul li:nth-of-type(" + i + ")";
-		$(".questionDiv ul").append("<li></li>"); //lägger till en li
-		$(li).text(questions.question1["answer" + (i - 1)]); //ger li:n text från korrekt fråga
+		$(".questionDiv ul").append("<li><span></span></li>"); //lägger till en li
+		$(li + " span").text(questions.question1["answer" + (i - 1)]); //ger li:n text från korrekt fråga
 		$(li).on("click",function() { //onclick style
-			$(".questionDiv ul li").removeClass("selected");
+			var thisIndex = $(this).index(); - 1;
+			$(".questionDiv ul li").removeClass("selected border");
 			$(this).addClass("selected");
-			// $(".questionDiv ul li:nth-of-type(" + (i - 2) + ")").addClass("border");
+			$(".questionDiv ul li:nth-of-type(" + thisIndex + ")").addClass("border");
 		});
 	}
 }
